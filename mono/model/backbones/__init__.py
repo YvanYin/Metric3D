00ce351aec8fadc67316d903f4cfa53a378f6e00
JUGGHM <35299633+JUGGHM@users.noreply.github.com>
2024-04-26 11:44:28 +0800

    Update __init__.py

--- mono/model/backbones/__init__.py
+++ mono/model/backbones/__init__.py
@@ -4,8 +4,8 @@ from .ConvNeXt import convnext_base
 from .ConvNeXt import convnext_large
 from .ConvNeXt import convnext_tiny
 from .ViT_DINO import vit_large
-from .ViT_DINO_reg import vit_small_reg, vit_large_reg
+from .ViT_DINO_reg import vit_small_reg, vit_large_reg, vit_giant2_reg
 
 __all__ = [
-    'convnext_xlarge', 'convnext_small', 'convnext_base', 'convnext_large', 'convnext_tiny', 'vit_small_reg', 'vit_large_reg'
+    'convnext_xlarge', 'convnext_small', 'convnext_base', 'convnext_large', 'convnext_tiny', 'vit_small_reg', 'vit_large_reg', 'vit_giant2_reg'
 ]
