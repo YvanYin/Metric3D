2b63f1f280177168eb371ad6306737a74aa160e6
Yuxuan Liu <uken.ryu.2@tier4.jp>
2024-06-03 09:53:35 +0900

    auto-formatting
    Signed-off-by: Yuxuan Liu <uken.ryu.2@tier4.jp>

--- onnx/metric3d_onnx_export.py
+++ onnx/metric3d_onnx_export.py
@@ -4,15 +4,21 @@ Export the torch hub model to ONNX format. Normalization is done in the model.
 
 import torch
 
+
 class Metric3DExportModel(torch.nn.Module):
     """
     The model for exporting to ONNX format. Add custom preprocessing and postprocessing here.
     """
+
     def __init__(self, meta_arch):
         super().__init__()
         self.meta_arch = meta_arch
-        self.register_buffer('rgb_mean', torch.tensor([123.675, 116.28, 103.53]).view(1, 3, 1, 1).cuda())
-        self.register_buffer('rgb_std', torch.tensor([58.395, 57.12, 57.375]).view(1, 3, 1, 1).cuda())
+        self.register_buffer(
+            "rgb_mean", torch.tensor([123.675, 116.28, 103.53]).view(1, 3, 1, 1).cuda()
+        )
+        self.register_buffer(
+            "rgb_std", torch.tensor([58.395, 57.12, 57.375]).view(1, 3, 1, 1).cuda()
+        )
         self.input_size = (616, 1064)
 
     def normalize_image(self, image):
@@ -20,19 +26,22 @@ class Metric3DExportModel(torch.nn.Module):
         image = image / self.rgb_std
         return image
 
-
     def forward(self, image):
         image = self.normalize_image(image)
         with torch.no_grad():
-            pred_depth, confidence, output_dict = self.meta_arch.inference({'input': image})
+            pred_depth, confidence, output_dict = self.meta_arch.inference(
+                {"input": image}
+            )
         return pred_depth
-        
+
+
 def update_vit_sampling(model):
     """
     For ViT models running on some TensorRT version, we need to change the interpolation method from bicubic to bilinear.
     """
     import torch.nn as nn
     import math
+
     def interpolate_pos_encoding_bilinear(self, x, w, h):
         previous_dtype = x.dtype
         npatch = x.shape[1] - 1
@@ -52,7 +61,9 @@ def update_vit_sampling(model):
         sqrt_N = math.sqrt(N)
         sx, sy = float(w0) / sqrt_N, float(h0) / sqrt_N
         patch_pos_embed = nn.functional.interpolate(
-            patch_pos_embed.reshape(1, int(sqrt_N), int(sqrt_N), dim).permute(0, 3, 1, 2),
+            patch_pos_embed.reshape(1, int(sqrt_N), int(sqrt_N), dim).permute(
+                0, 3, 1, 2
+            ),
             scale_factor=(sx, sy),
             mode="bilinear",  # Change from bicubic to bilinear
             antialias=self.interpolate_antialias,
@@ -61,13 +72,20 @@ def update_vit_sampling(model):
         assert int(w0) == patch_pos_embed.shape[-2]
         assert int(h0) == patch_pos_embed.shape[-1]
         patch_pos_embed = patch_pos_embed.permute(0, 2, 3, 1).view(1, -1, dim)
-        return torch.cat((class_pos_embed.unsqueeze(0), patch_pos_embed), dim=1).to(previous_dtype)
+        return torch.cat((class_pos_embed.unsqueeze(0), patch_pos_embed), dim=1).to(
+            previous_dtype
+        )
 
-    model.depth_model.encoder.interpolate_pos_encoding = interpolate_pos_encoding_bilinear.__get__(model.depth_model.encoder, model.depth_model.encoder.__class__)
+    model.depth_model.encoder.interpolate_pos_encoding = (
+        interpolate_pos_encoding_bilinear.__get__(
+            model.depth_model.encoder, model.depth_model.encoder.__class__
+        )
+    )
     return model
 
-def main(model_name='metric3d_vit_small', modify_upsample=False):
-    model = torch.hub.load('yvanyin/metric3d', model_name, pretrain=True)
+
+def main(model_name="metric3d_vit_small", modify_upsample=False):
+    model = torch.hub.load("yvanyin/metric3d", model_name, pretrain=True)
     model.cuda().eval()
 
     if modify_upsample:
@@ -84,13 +102,18 @@ def main(model_name='metric3d_vit_small', modify_upsample=False):
     export_model.cuda()
 
     onnx_output = f"{model_name}.onnx"
-    dummy_input = (dummy_image, )
-    torch.onnx.export(export_model, dummy_input, onnx_output,
-        input_names=['image'],
-        output_names=['pred_depth'], opset_version=11,
+    dummy_input = (dummy_image,)
+    torch.onnx.export(
+        export_model,
+        dummy_input,
+        onnx_output,
+        input_names=["image"],
+        output_names=["pred_depth"],
+        opset_version=11,
     )
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     from fire import Fire
+
     Fire(main)
